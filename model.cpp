c855c632ea1de39b09099cc8ce7932d246d8ffa3
Kinh Nguyen <fieeuvaan@gmail.com>
2021-06-11 16:22:51 +0200

    retab

--- model.cpp
+++ model.cpp
@@ -1,7 +1,7 @@
 #include <TMB.hpp>
 #include "ktools.hpp"
 
-template<class Type>
+  template<class Type>
 Type objective_function<Type>::operator() ()
 {
   parallel_accumulator<Type> dll(this);
@@ -16,20 +16,20 @@ Type objective_function<Type>::operator() ()
 
   DATA_VECTOR(svw);
 
-	// meta data
-	DATA_INTEGER(to_skew);
-	DATA_INTEGER(age_term);
-	DATA_INTEGER(yob_term);
-	DATA_INTEGER(smooth_age);
-	DATA_INTEGER(smooth_yob);
-	DATA_INTEGER(interval_censor);
-	DATA_IVECTOR(to_fit);
+  // meta data
+  DATA_INTEGER(to_skew);
+  DATA_INTEGER(age_term);
+  DATA_INTEGER(yob_term);
+  DATA_INTEGER(smooth_age);
+  DATA_INTEGER(smooth_yob);
+  DATA_INTEGER(interval_censor);
+  DATA_IVECTOR(to_fit);
 
   // priors
   DATA_VECTOR(sd_beta);
   DATA_VECTOR(sd_yob);
   DATA_VECTOR(sd_age);
-  
+
   DATA_SCALAR(age_order);
   DATA_SCALAR(yob_order);
 
@@ -52,87 +52,87 @@ Type objective_function<Type>::operator() ()
 
   // Skewness *
   PARAMETER(log_skew);
-	Type skew = 1.;
-	if (to_skew) {
-		prior -= dnorm(log_skew, skew_prior(0), skew_prior(1), true);
-		// Skewness real
-		/* a = exp(skew - 1.1*log_shape); */
-		skew = exp(log_skew);
-	}
+  Type skew = 1.;
+  if (to_skew) {
+    prior -= dnorm(log_skew, skew_prior(0), skew_prior(1), true);
+    // Skewness real
+    /* a = exp(skew - 1.1*log_shape); */
+    skew = exp(log_skew);
+  }
 
   // yob rw2
-	PARAMETER 			 (beta_yob);
+  PARAMETER                (beta_yob);
   PARAMETER_VECTOR (yob_rw2);
   PARAMETER        (log_yob_rw2_e);
 
-	if (yob_term) 
-	{
-		if (smooth_yob) {
-			Type yob_rw2_e = exp(log_yob_rw2_e);
-			prior -= ktools::pc_prec(yob_rw2_e, sd_yob(0), sd_yob(1));
-			prior += ktools::rw(yob_rw2, R_yob, yob_rw2_e, yob_order);
-		} else {
-			prior -= dnorm(beta_yob, sd_beta(0), sd_beta(1), true);
-		}
-	}
+  if (yob_term) 
+  {
+    if (smooth_yob) {
+      Type yob_rw2_e = exp(log_yob_rw2_e);
+      prior -= ktools::pc_prec(yob_rw2_e, sd_yob(0), sd_yob(1));
+      prior += ktools::rw(yob_rw2, R_yob, yob_rw2_e, yob_order);
+    } else {
+      prior -= dnorm(beta_yob, sd_beta(0), sd_beta(1), true);
+    }
+  }
 
   // age rw2
-	PARAMETER 			 (beta_age);
+  PARAMETER                (beta_age);
   PARAMETER_VECTOR (age_rw2);
   PARAMETER        (log_age_rw2_e);
 
-	if (age_term)
-	{
-		if (smooth_age) {
-			Type age_rw2_e = exp(log_age_rw2_e);
-			prior -= ktools::pc_prec(age_rw2_e, sd_age(0), sd_age(1));
-			prior += ktools::rw(age_rw2, R_age, age_rw2_e, age_order);
-		} else {
-			prior -= dnorm(beta_age, sd_beta(0), sd_beta(1), true);
-		}
-	}
+  if (age_term)
+  {
+    if (smooth_age) {
+      Type age_rw2_e = exp(log_age_rw2_e);
+      prior -= ktools::pc_prec(age_rw2_e, sd_age(0), sd_age(1));
+      prior += ktools::rw(age_rw2, R_age, age_rw2_e, age_order);
+    } else {
+      prior -= dnorm(beta_age, sd_beta(0), sd_beta(1), true);
+    }
+  }
 
   // Data likelihood
-	vector<Type> logliki(afs.size());
+  vector<Type> logliki(afs.size());
 
   for (int i = 0; i < afs.size(); i++) {
 
     Type eta = intercept;
 
-		if (yob_term) 
-		{
-			if (smooth_yob)
-				eta += yob_rw2(yob(i));
-			else
-				eta += beta_yob * yob(i);
-		}
+    if (yob_term) 
+    {
+      if (smooth_yob)
+        eta += yob_rw2(yob(i));
+      else
+        eta += beta_yob * yob(i);
+    }
 
-		if (age_term)
-		{
-			if (smooth_age)
-				eta += age_rw2(age(i));
-			else
-				eta += beta_age * age(i);
-		}
+    if (age_term)
+    {
+      if (smooth_age)
+        eta += age_rw2(age(i));
+      else
+        eta += beta_age * age(i);
+    }
 
     Type scale = exp(eta);
 
     if (event(i)) 
-		{
-			if (interval_censor)
-				logliki(i) = log( svw(i) * (
-							ktools::St_llogisI(afs_l(i), shape, scale, skew) - 
-							ktools::St_llogisI(afs_u(i), shape, scale, skew) ));
-			else
-				logliki(i) = log( svw(i) * ktools::ft_llogisI(afs(i), shape, scale, skew) );
+    {
+      if (interval_censor)
+        logliki(i) = log( svw(i) * (
+              ktools::St_llogisI(afs_l(i), shape, scale, skew) - 
+              ktools::St_llogisI(afs_u(i), shape, scale, skew) ));
+      else
+        logliki(i) = log( svw(i) * ktools::ft_llogisI(afs(i), shape, scale, skew) );
     } 
-		else {
+    else {
       logliki(i) = log(svw(i) * ktools::St_llogisI(afs(i), shape, scale, skew));
     }
-		if (to_fit(i))
-			dll -= logliki(i);
+    if (to_fit(i))
+      dll -= logliki(i);
   }
   dll += prior;
-	REPORT(logliki);
+  REPORT(logliki);
   return dll;
 }
